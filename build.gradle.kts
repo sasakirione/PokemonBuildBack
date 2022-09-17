c385a46231f2074dd6beb1994a5b3c3be681b5ad
sasakirione <sasakirione@gmail.com>
2022-09-17 18:58:42 +0900

    Update: ライブラリアップデート対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,16 +33,23 @@ dependencies {
     implementation("io.ktor:ktor-auth:1.6.8")
     implementation("io.ktor:ktor-auth-jwt:1.6.8")
     implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
-    implementation("org.jetbrains.exposed:exposed-core:0.38.2")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.38.2")
-    implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
-    implementation("org.postgresql:postgresql:42.3.6")
+    implementation("org.jetbrains.exposed:exposed-core:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.39.2")
+    implementation("org.postgresql:postgresql:42.5.0")
     implementation("io.insert-koin:koin-core:3.2.0")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-server-cors:2.0.3")
+    implementation("io.ktor:ktor-server-cors:2.1.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+kotlin.sourceSets {
+    main {
+        kotlin.srcDir("src/main/kotlin")
+        resources.srcDir("src/main/resources")
+    }
 }
 
 
