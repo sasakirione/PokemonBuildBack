499fefef5d55d16ebfbae90ee7e8f4772cdcb2d8
sasakirione <sasakirione@gmail.com>
2022-10-30 22:12:21 +0900

    etc: バージョンアップ対応2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
